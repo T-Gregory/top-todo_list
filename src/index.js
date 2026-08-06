fa4f310f883b17ef9deee06d7055c54535c1c79c
T-Gregory <gregth42@gmail.com>
2026-08-06 19:06:36 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./styles.css";
+
+let body = document.querySelector("body");
+let msgContainer = document.createElement("p");
+msgContainer.textContent = "Hello world !";
+body.appendChild(msgContainer);

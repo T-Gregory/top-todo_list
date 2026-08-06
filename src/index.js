b906fae5a1c6f069e3c488fc3628e60c508f6639
greg <gregth42@gmail.com>
2026-08-06 19:10:20 +0200

    chore: Project initial commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
 import "./styles.css";
 
-let body = document.querySelector("body");
-let msgContainer = document.createElement("p");
-msgContainer.textContent = "Hello world !";
-body.appendChild(msgContainer);
+

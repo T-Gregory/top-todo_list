e69b92918102b8dd4ffd352dedc373c69ae88a5a
greg <gregth42@gmail.com>
2026-08-13 15:53:10 +0200

    refacto: Add styling to project creation button

--- src/display/elements/projectCreationButton/projectCreationButton.js
+++ src/display/elements/projectCreationButton/projectCreationButton.js
@@ -1,9 +1,15 @@
 import { createProjectCreationModal } from "#display/modals/projectCreationModal.js";
 import { triggerStateChangeEvent } from "#display/event.js";
 
+import "./style.css";
+
+
+const PROJECT_CREATION_BUTTON_CONTAINER_CLASS_NAME = "project-creation-button-container";
 
 function getProjectCreationButton(app) {
     const projectCreationButtonContainer = document.createElement("div");
+    projectCreationButtonContainer.classList.add(PROJECT_CREATION_BUTTON_CONTAINER_CLASS_NAME);
+
     const projectCreationButton = document.createElement("button");
     projectCreationButton.textContent = "New project";
 
